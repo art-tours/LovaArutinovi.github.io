22124b24821ce0f178504947f7299af5600e8583
LovaArutinovi <arutinov58@gmail.com>
2021-01-13 20:49:47 +0400

    13.01.2021

---
+++ old/js/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+    $(function(){
+        let header = $('.header'),
+            headerH = header.innerHeight(),
+            scrollOffset = $(window).scrollTop();
+            
+        /* Fixed Header */
+        // checkSkroll(scrollOffset);
+        $(window).on("scroll", function(){
+            scrollOffset = $(this).scrollTop();
+
+            checkSkroll(scrollOffset);
+        });
+        function checkSkroll(scrollOffset) {
+            if( scrollOffset >= headerH ) {
+               header.addClass("fixed");
+            } else {
+               header.removeClass("fixed");
+            }
+        }
+        $("[data-scroll]").on("click" , function(event){
+            event.preventDefault();
+      
+            var $this = $(this),
+                blockId = $(this).data('scroll'),
+                blockOffset = $(blockId).offset().top - headerH;
+
+                // console.log(blockOffset)
+      
+            // $("#nav a").removeClass("active");
+            // $this.addClass("active");
+      
+            $("html, body").animate({
+               scrollTop: blockOffset
+            },1000);
+         });
+    });
+
+    document.querySelector('.header__burger').onclick = function(){
+        this.classList.toggle('active');
+        document.querySelector('.nav').classList.toggle('active');
+    }
+});
